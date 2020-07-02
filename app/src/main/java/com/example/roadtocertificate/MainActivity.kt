af72e86ccaba1a71cf6d448ba56eda1fbb3d002d
thanakritju <thanakrit.ju.work@gmail.comm>
2020-07-02 14:13:39 +0700

    [Android Kotlin Fundamentals: Image resources and compatibility] code challenge

--- app/src/main/java/com/example/roadtocertificate/MainActivity.kt
+++ app/src/main/java/com/example/roadtocertificate/MainActivity.kt
@@ -7,12 +7,14 @@ import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var diceImage: ImageView
+    private lateinit var diceImage2: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         diceImage = findViewById(R.id.dice_image)
+        diceImage2 = findViewById(R.id.dice_image_2)
 
         val rollButton: Button = findViewById(R.id.roll_button)
         val resetButton: Button = findViewById(R.id.reset_button)
@@ -27,11 +29,16 @@ class MainActivity : AppCompatActivity() {
 
     private fun reset() {
         diceImage.setImageResource(R.drawable.empty_dice)
+        diceImage2.setImageResource(R.drawable.empty_dice)
     }
 
     private fun rollDice() {
-        val randomInt = (1..6).random()
-        val drawableResource = when (randomInt) {
+        diceImage.setImageResource(getRandomDiceImage())
+        diceImage2.setImageResource(getRandomDiceImage())
+    }
+
+    private fun getRandomDiceImage() : Int {
+        return when ((1..6).random()) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -39,6 +46,5 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        diceImage.setImageResource(drawableResource)
     }
 }
