cfaebb903d8eca1e9593ca0b920de5d46654188f
thanakritju <thanakrit.ju.work@gmail.comm>
2020-07-02 14:06:56 +0700

    [Android Kotlin Fundamentals: Image resources and compatibility] learn backward compat and reduce size of app by using appcompat

--- app/src/main/java/com/example/roadtocertificate/MainActivity.kt
+++ app/src/main/java/com/example/roadtocertificate/MainActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var diceImage: ImageView
