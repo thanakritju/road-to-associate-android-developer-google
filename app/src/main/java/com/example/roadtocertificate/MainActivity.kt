cb90a7a63cb898b199f3d1917d33a8a49b641a9a
thanakritju <thanakrit.ju.work@gmail.comm>
2020-07-02 15:20:11 +0700

    [Android Kotlin Fundamentals 02.1: Linear layout using the Layout Editor] reset previos code

--- app/src/main/java/com/example/roadtocertificate/MainActivity.kt
+++ app/src/main/java/com/example/roadtocertificate/MainActivity.kt
@@ -2,49 +2,11 @@ package com.example.roadtocertificate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var diceImage: ImageView
-    private lateinit var diceImage2: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        diceImage = findViewById(R.id.dice_image)
-        diceImage2 = findViewById(R.id.dice_image_2)
-
-        val rollButton: Button = findViewById(R.id.roll_button)
-        val resetButton: Button = findViewById(R.id.reset_button)
-
-        rollButton.setOnClickListener {
-            rollDice()
-        }
-        resetButton.setOnClickListener {
-            reset()
-        }
-    }
-
-    private fun reset() {
-        diceImage.setImageResource(R.drawable.empty_dice)
-        diceImage2.setImageResource(R.drawable.empty_dice)
-    }
-
-    private fun rollDice() {
-        diceImage.setImageResource(getRandomDiceImage())
-        diceImage2.setImageResource(getRandomDiceImage())
-    }
-
-    private fun getRandomDiceImage() : Int {
-        return when ((1..6).random()) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
-        }
     }
 }
