2a881b91f31e1d818e67da070250e567b2e1c55c
thanakritju <thanakrit.ju.work@gmail.comm>
2020-07-03 10:35:29 +0700

    Revert "[Android Kotlin Fundamentals 02.2: Add user interactivity]nickname editable, learn highlight text and show keyboard"
    This reverts commit 7e32bed4056cdf347ce539d4530993257a40bf1e.

--- app/src/main/java/com/example/roadtocertificate/MainActivity.kt
+++ app/src/main/java/com/example/roadtocertificate/MainActivity.kt
@@ -1,58 +1,36 @@
 package com.example.roadtocertificate
 
-import android.graphics.Color
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setListeners()
-    }
 
-    private fun makeColored(view: View) {
-        when (view.id) {
-
-            // Boxes using Color class colors for the background
-            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
-            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
-            R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
-            R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
-            R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
-            // Boxes using custom colors for background
-            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
-            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
-            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
-            else -> view.setBackgroundColor(Color.LTGRAY)
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            addNickname(it)
         }
     }
 
-    private fun setListeners() {
+    private fun addNickname(view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+        nicknameTextView.text = editText.text
 
-        val boxOneText = findViewById<TextView>(R.id.box_one_text)
-        val boxTwoText = findViewById<TextView>(R.id.box_two_text)
-        val boxThreeText = findViewById<TextView>(R.id.box_three_text)
-        val boxFourText = findViewById<TextView>(R.id.box_four_text)
-        val boxFiveText = findViewById<TextView>(R.id.box_five_text)
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
 
-        val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
-
-        val redButton = findViewById<TextView>(R.id.red_button)
-        val greenButton = findViewById<TextView>(R.id.green_button)
-        val yellowButton = findViewById<TextView>(R.id.yellow_button)
-
-        val clickableViews: List<View> =
-            listOf(boxOneText, boxTwoText, boxThreeText,
-                boxFourText, boxFiveText, rootConstraintLayout,
-                redButton, greenButton, yellowButton)
-
-        for (item in clickableViews) {
-            item.setOnClickListener { makeColored(it) }
-        }
+        // Hide the keyboard.
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
