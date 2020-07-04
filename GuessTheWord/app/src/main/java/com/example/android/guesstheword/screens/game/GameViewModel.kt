0dde78c6e29b80aecbbc531f3c0813044eb09da7
thanakritju <thanakrit.ju.work@gmail.com>
2020-07-04 12:13:38 +0700

    [Android Kotlin Fundamentals 05.2: LiveData and LiveData observers] implement end game logic

--- GuessTheWord/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ GuessTheWord/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -17,6 +17,10 @@ class GameViewModel: ViewModel() {
     val score: LiveData<Int>
         get() = _score
 
+    private val _eventGameFinish = MutableLiveData<Boolean>()
+    val eventGameFinish: LiveData<Boolean>
+        get() = _eventGameFinish
+
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
 
@@ -48,7 +52,9 @@ class GameViewModel: ViewModel() {
      * Moves to the next word in the list
      */
     private fun nextWord() {
-        if (!wordList.isEmpty()) {
+        if (wordList.isEmpty()) {
+            onGameFinish()
+        } else {
             //Select and remove a word from the list
             _word.value = wordList.removeAt(0)
         }
@@ -82,5 +88,14 @@ class GameViewModel: ViewModel() {
                 "bubble"
         )
         wordList.shuffle()
+    }
+
+    /** Method for the game completed event **/
+    private fun onGameFinish() {
+        _eventGameFinish.value = true
+    }
+
+    fun onGameFinishComplete() {
+        _eventGameFinish.value = false
     }
 }
